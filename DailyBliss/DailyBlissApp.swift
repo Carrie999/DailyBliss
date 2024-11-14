89bbacb8843368ead5d9f08f237104097153bc1c
carrie <771685809@qq.com>
2024-11-14 11:14:43 +0800

    Initial Commit

---
+++ DailyBliss/DailyBlissApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyBlissApp.swift
+//  DailyBliss
+//
+//  Created by  玉城 on 2024/11/14.
+//
+
+import SwiftUI
+
+@main
+struct DailyBlissApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
